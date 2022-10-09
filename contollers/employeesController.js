adc7f04d50aad71a6d084901418fc8b0aed86d65
imeshujith <[Ime$h]84>
2022-10-10 00:54:22 +0530

    fix some bugs

--- contollers/employeesController.js
+++ contollers/employeesController.js
@@ -3,41 +3,50 @@ const db = require("../models");
 // create main Model
 const Employee = db.employees;
 
-// 1. create product
+// 1. create employee
 const addEmployee = async (req, res, err) => {
-  const employee = await Employee.bulkCreate(req.body, {
-    fields: ["id", "login", "name", "salary"],
-    updateOnDuplicate: ["login", "name", "salary"],
-  });
-  res.status(201).send(employee);
+  try {
+    const employee = await Employee.bulkCreate(req.body, {
+      fields: ["id", "login", "name", "salary"],
+      updateOnDuplicate: ["login", "name", "salary"],
+    });
+    res.status(201).send(employee);
+  } catch (e) {
+    res.status(400).send("Duplication error. Login cannot be duplicate");
+  }
 };
 
-// 2. get all products
+// 2. get all employees
 const getAllEmployees = async (req, res) => {
-  let employee = await Employee.findAll({});
-  res.status(200).send(employee);
-};
-
-// 3. get single product
-const getOneEmployee = async (req, res) => {
-  let id = req.params.id;
-  let product = await Employee.findOne({ where: { id: id } });
-  res.status(200).send(product);
+  try {
+    let employee = await Employee.findAll({});
+    res.status(200).send(employee);
+  } catch (e) {
+    res.status(400).send("Server error");
+  }
 };
 
 // 4. update Employee
 const updateEmployee = async (req, res) => {
-  let id = req.params.id;
-  const product = await Employee.update(req.body, { where: { id: id } });
-  res.status(204).send(product);
+  try {
+    let id = req.params.id;
+    const employee = await Employee.update(req.body, { where: { id: id } });
+    res.status(204).send(employee);
+  } catch (e) {
+    res.status(400).send("Server error");
+  }
 };
 
-// 5. delete product by id
+// 5. delete employee by id
 const deleteEmployee = async (req, res) => {
-  let id = req.params.id;
-  await Employee.destroy({ where: { id: id } });
-  let employee = await Employee.findAll({});
-  res.status(204).send();
+  try {
+    let id = req.params.id;
+    await Employee.destroy({ where: { id: id } });
+    let employee = await Employee.findAll({});
+    res.status(204).send();
+  } catch (e) {
+    res.status(400).send("Server error");
+  }
 };
 
 module.exports = {
